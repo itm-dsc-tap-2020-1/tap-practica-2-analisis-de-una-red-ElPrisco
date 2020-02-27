e033a6d83d091aceac9940bf81c2a0a38acab6cd
ElPrisco <61329579+ElPrisco@users.noreply.github.com>
2020-02-27 11:52:40 -0600

    Add files via upload

---
+++ Seguridad.py
@@ -0,0 +1,21 @@
+import os
+
+red = "200.33.171.0/24"
+os.system("nmap -sP "+ red)
+
+#Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 11:47 CST
+#Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+#Host is up (0.0037s latency).
+#Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+#Host is up (0.0020s latency).
+#Nmap scan report for 200.33.171.13
+#Host is up (0.0021s latency).
+#Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+#Host is up (0.0036s latency).
+#Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+#Host is up (0.0055s latency).
+#Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+#Host is up (0.0016s latency).
+#Nmap scan report for 200.33.171.254
+#Host is up (0.0028s latency).
+#Nmap done: 256 IP addresses (7 hosts up) scanned in 4.42 seconds
